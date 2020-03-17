c63cade63c9ebf082edbde5dd52d581a3749d052
Manish Shrivastava <41223158+manish6007@users.noreply.github.com>
2020-03-17 15:50:48 +0530

    Update random_teams_generation.py

--- random_teams_generation.py
+++ random_teams_generation.py
@@ -1,7 +1,14 @@
 import random
+from termcolor import colored
+
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+filelocation = askopenfilename()  # open the dialog GUI
 
 l = []
-f = open("player_list.txt", "r+")
+f = open(filelocation, "r+")
 
 for name in f:
     l = name.split(',')
@@ -10,8 +17,22 @@ for i in range(4):
     random.shuffle(l)
 
 teams = []
-for i in range(0,  len(l), 2):
+for i in range(0, len(l), 2):
     teams.append(l[i:i + 2])
 
+print(colored("\nTeams are as follows:\n", 'blue'))
 for i in range(0, len(teams)):
-    print("Team {}: {} & {}".format(i, teams[i][0],teams[i][1]))
+    print(colored("Team {}: {} & {}".format(i + 1, teams[i][0], teams[i][1]), 'magenta'))
+
+for i in range(4):
+    random.shuffle(teams)
+
+matches = []
+for i in range(0, len(teams), 2):
+    matches.append(teams[i:i + 2])
+
+print(colored("\nMatches are as follows:\n", 'blue'))
+for i in range(0, len(matches)):
+    print(colored(
+        "\nMatch {}: **{} & {}** Vs **{} & {}**".format(i + 1, matches[i][0][0], matches[i][0][1], matches[i][1][0],
+                                                        matches[i][1][1]), 'green'))
